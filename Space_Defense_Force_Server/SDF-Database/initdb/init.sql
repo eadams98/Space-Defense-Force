0ee6857c2abf3b6c23a2fed7dfc6b0ef29a5292f
eadams98 <eric051598@gmail.com>
2025-01-05 15:19:01 -0600

    Config changes

--- Space_Defense_Force_Server/SDF-Database/initdb/init.sql
+++ Space_Defense_Force_Server/SDF-Database/initdb/init.sql
@@ -1,2 +1,22 @@
 CREATE DATABASE IF NOT EXISTS defense_force;
 
+use mysql;
+-- Ensure the root user exists and has the correct password
+CREATE USER IF NOT EXISTS 'sdf'@'%' IDENTIFIED BY 'root';
+CREATE USER IF NOT EXISTS 'sdf'@'localhost' IDENTIFIED BY 'root';
+-- CREATE USER IF NOT EXISTS 'root'@'%' IDENTIFIED BY 'root';
+-- CREATE USER IF NOT EXISTS 'root'@'localhost' IDENTIFIED BY 'root';
+
+-- Reset the root user's password in case it's overwritten
+ALTER USER 'sdf'@'%' IDENTIFIED BY 'root';
+ALTER USER 'sdf'@'localhost' IDENTIFIED BY 'root';
+-- ALTER USER 'root'@'%' IDENTIFIED BY 'root';
+-- ALTER USER 'root'@'localhost' IDENTIFIED BY 'root';
+
+-- Grant full privileges to the root user
+GRANT ALL PRIVILEGES ON *.* TO 'sdf'@'%';
+GRANT ALL PRIVILEGES ON *.* TO 'sdf'@'localhost';
+-- GRANT ALL PRIVILEGES ON *.* TO 'root'@'%';
+-- GRANT ALL PRIVILEGES ON *.* TO 'root'@'localhost';
+
+FLUSH PRIVILEGES;
